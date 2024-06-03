f7d1446c294809ae2f53f67a537bff3d00990975
Your Name <you@example.com>
2024-06-03 09:54:59 +0200

    25

--- read.php
+++ read.php
@@ -1,7 +1,29 @@
 <?php 
+    include 'db.php';
     include 'header.php';
+    $sql = "SELECT id, nombre, telefono, correo, mensaje FROM contactos";
+    $result=$conn->query($sql);
 ?>
+
 <h1>Mensajes</h1>
+
+<div class="contenedor listado">
+    <?php
+        while($row = $result->fetch_assoc()){
+            ?>
+            <div class="fila">
+                    <div class="campo"><?=$row['id']?></div>
+                    <div class="campo"><?=$row['nombre']?></div>
+                    <div class="campo"><?=$row['telefono']?></div>
+                    <div class="campo"><?=$row['correo']?></div>
+                    <div class="campo"><?=$row['mensaje']?></div>
+            </div>
+
+        <?php
+        }
+    ?>
+</div>
+
 <?php
     include 'footer.php';
 ?>
