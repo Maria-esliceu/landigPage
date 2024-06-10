14b622fc2a7359696a5fb8c9ac525c9435a1bbc2
Your Name <you@example.com>
2024-06-10 09:20:18 +0200

    prova

---
+++ borrar.php
@@ -0,0 +1,13 @@
+<?php
+include "db.php";
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    $id=$_GET['id'];
+    $sql = "DELETE FROM contactos WHERE id=?";
+    $stmt = $conn->prepare($sql);
+
+    //Vincular el parámetro y ejecutar la declaración
+    $stmt->bind_param("i", $id);
+    $result = $stmt->execute();
+    $msg="Registro borrado con exito";
+    header("Location: read.php?status=success&msg=" .$msg);
+}
--- read.php
+++ read.php
@@ -24,16 +24,29 @@
         <div class="campoCabecera">
             Mensaje
         </div>
+        <div class="campoCabecera">
+            Acción
+        </div>
     </div>
     <?php
+        $num=0;
         while($row = $result->fetch_assoc()){
+            $num++;
+            $paridad="impar";
+            if($num%2==0){
+                $paridad="par";
+            }
+            //PARIDAT ME INDICA SI LA FILA ES PAR O IMPAR
             ?>
-            <div class="fila">
+            <div class="fila <?=$paridad?>">
                     <div class="campo"><?=$row['id']?></div>
                     <div class="campo"><?=$row['nombre']?></div>
                     <div class="campo"><?=$row['telefono']?></div>
                     <div class="campo"><?=$row['correo']?></div>
                     <div class="campo"><?=$row['mensaje']?></div>
+                    <div class="btn-borrar">
+                        <a href="borrar.php?id=<?=$row['id']?>">Borrar</a>
+                    </div>
             </div>
 
         <?php
